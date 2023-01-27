3c3f68bd7c31735a4a0735ec65caa84f24b805d4
Cobey Hollier <cobey.hollier@mac.com>
2023-01-27 15:22:44 +0100

    CH: Fixing errors

--- Assets/Scripts/PlayerGun.cs
+++ Assets/Scripts/PlayerGun.cs
@@ -2,8 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PlayerGun : MonoBehaviour
+public class PlayerGun : NetworkBehaviour
 {
     [SerializeField]
     private GameObject gun;
