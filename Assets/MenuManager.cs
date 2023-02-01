11bc69188e62490d4159d0e837183b3c7b8dfd74
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2023-02-01 20:53:28 +0300

    KC: player network stuff

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MenuManager : NetworkBehaviour
+{
+    public GameObject menuFrame;
+    public GameObject waitingFrame;
+    public GameObject upgradeFrame;
+
+    bool gameStarted = false;
+
+    public void OpenWaitingFrame()
+    {
+        menuFrame.SetActive(false);
+        waitingFrame.SetActive(true);
+    }
+
+    public void Awake()
+    {
+        menuFrame = GameObject.Find("MenuFrame");
+        waitingFrame = GameObject.Find("WaitingFrame");
+        upgradeFrame = GameObject.Find("UpgradeFrame");
+        
+    }
+
+    [Server]
+    void Update()
+    {
+        if (gameStarted)
+            return;
+
+        Debug.Log("gamemanager update");
+
+        if (NetworkServer.connections.Count > 1)
+        {
+            OpenUpgradeFrame();
+            gameStarted = true;
+        }
+    }
+
+    [ClientRpc]
+    void OpenUpgradeFrame()
+    {
+        Debug.Log("open upgrade frame ran");
+
+        menuFrame.SetActive(false);
+        waitingFrame.SetActive(false);
+        upgradeFrame.SetActive(true);
+    }
+}
