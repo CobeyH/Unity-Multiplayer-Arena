fb344a8a3a166d06f72f195dc34458d57d52c632
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2023-02-01 19:57:43 +0300

    KC: network stuff menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using Mirror;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
-    public void ResetGame()
+    public GameObject menuFrame;
+    public GameObject waitingFrame;
+    public GameObject upgradeFrame;
+
+    bool gameStarted = false;
+
+    public void OpenWaitingFrame()
     {
-        SceneManager.LoadScene("SampleScene");
+        menuFrame.SetActive(false);
+        waitingFrame.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (!isServer || gameStarted)
+            return;
+
+        if (NetworkServer.connections.Count > 1)
+        {
+            OpenUpgradeFrame();
+            gameStarted = true;
+        }
+    }
+
+    [ClientRpc]
+    void OpenUpgradeFrame()
+    {
+        waitingFrame.SetActive(false);
+        upgradeFrame.SetActive(true);
     }
 }
